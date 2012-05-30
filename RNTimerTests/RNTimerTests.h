ded799b35e10f76f318f7c5095b9db20367076c6
Rob Napier <robnapier@gmail.com>
2012-05-30 10:09:39 -0400

    Add test cases and build as library

---
+++ RNTimerTests/RNTimerTests.h
@@ -0,0 +1,13 @@
+//
+//  RNTimerTests.h
+//  RNTimerTests
+//
+//  Created by Rob Napier on 05/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RNTimerTests : SenTestCase
+
+@end
---
+++ RNTimerTests/RNTimerTests.m
@@ -0,0 +1,58 @@
+//
+//  RNTimerTests.m
+//  RNTimerTests
+//
+//  Created by Rob Napier on 05/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RNTimerTests.h"
+#import "RNTimer.h"
+
+@implementation RNTimerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testSimple
+{
+  RNTimer *timer = [RNTimer repeatingTimerWithTimeInterval:1 block:^{}];
+  STAssertNotNil(timer, @"Timer must not be nil");
+}
+
+- (void)testDoesFire
+{
+  __block BOOL didFire = NO;
+  RNTimer *timer = [RNTimer repeatingTimerWithTimeInterval:1 block:^{
+    didFire = YES;
+  }];
+
+  [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:1]];
+  STAssertTrue(didFire, @"Didn't fire:%@", timer);
+}
+
+- (void)testDoesntFireToSoon
+{
+  __block BOOL didFire = NO;
+  RNTimer *timer = [RNTimer repeatingTimerWithTimeInterval:1 block:^{
+    didFire = YES;
+  }];
+
+  [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:.5]];
+  STAssertFalse(didFire, @"Did fire:%@", timer);
+}
+
+
+
+@end
